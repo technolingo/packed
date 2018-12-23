94cb818488ea0fd0de73b36c6f59bb354648e48f
ZILONG LI <11096690+technolingo@users.noreply.github.com>
2018-12-23 16:06:08 +0900

    webpack configs for css modules

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const autoprefixer = require('autoprefixer');
 
 module.exports = {
   devtool: 'cheap-module-eval-source-map',
@@ -16,9 +17,36 @@ module.exports = {
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        use: {
-          loader: "babel-loader"
-        }
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.css/,
+        exclude: /node_modules/,
+        use: [
+          {loader: 'style-loader'},
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1,
+              module: true,
+              localIdentName: '[name]__[local]__[hash:base64:5]'
+            }
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              ident: 'postcss',
+              plugins: () => [
+                autoprefixer({
+                  browsers: [
+                    '> 1%',
+                    'last 2 versions'
+                  ]
+                })
+              ]
+            }
+          }
+        ]
       }
     ]
   }
