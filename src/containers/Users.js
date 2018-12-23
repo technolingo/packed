1dcc7d432c03100b14e67cd41223916f0602a841
ZILONG LI <11096690+technolingo@users.noreply.github.com>
2018-12-23 15:12:55 +0900

    containers

---
+++ src/containers/Users.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Users extends Component {
+  render () {
+    return (
+      <div>
+        <h1>Customers</h1>
+        <p>Most loyal users.</p>
+      </div>
+    );
+  }
+}
+
+export default Users;
